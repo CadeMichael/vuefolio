f14a8d376e6a88a8da83b0c51920350b42bbd086
Cade Lueker <cade@debPad.Home>
2023-12-01 07:12:08 -0700

    drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,6 @@
      const element = document.getElementById(componentId);
      if (element) {
          element.scrollIntoView({ behavior: 'smooth' });
-         document.activeElement.blur();
      } else {
     console.warn(`Element with ID ${componentId} not found`);
   }
@@ -16,38 +15,44 @@
 </script>
 
 <template>
-	<div class="navbar bg-neutral rounded shadow-xl">
-		<div class="navbar-start">
-			<div id="navdrop" class="dropdown">
-				<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-				</div>
-				<ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral rounded-box w-52">
-					<li><button @click="scrollToComponent('about')">About</button></li>
-					<li><button @click="scrollToComponent('projects')">Projects</button></li>
-					<li><button @click="scrollToComponent('resume')">Resume</button></li>
-				</ul>
-			</div>
-		<div tabindex="0" class="hidden lg:avatar">
-			<div class="w-10 mask mask-squircle">
-				<img :src="getImageUrl('profile','jpg')" alt="Tailwind CSS Navbar component" />
-			</div>
-		</div>
-		</div>
-		<div class="navbar-center">
-			<a class="ml-2 hidden lg:btn lg:btn-ghost lg:text-xl">Cade Lueker</a>
-		</div>
-		<div class="lg:hidden navbar-end">
-			<a class="lm-2 btn btn-ghost text-xl">Cade Lueker</a>
-		</div>
-		<div class="navbar-end hidden lg:flex">
-			<ul class="menu menu-horizontal px-1">
-				<li><a @click="scrollToComponent('about')">About</a></li>
-				<li><a @click="scrollToComponent('projects')">Projects</a></li>
-				<li><a @click="scrollToComponent('resume')">Resume</a></li>
-			</ul>
-		</div>
-	</div>
+    <div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
+        <div class="drawer-content">
+            <div class="navbar bg-neutral rounded shadow-xl">
+                <div class="flex-none lg:hidden">
+                    <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </label>
+                </div>
+                <div tabindex="0" class="hidden lg:avatar navbar-start">
+                    <div class="w-10 mask mask-squircle">
+                        <img :src="getImageUrl('profile','jpg')" alt="Tailwind CSS Navbar component" />
+                    </div>
+                </div>
+                <div class="navbar-center mx-auto">
+                    <a class="ml-2 hidden lg:btn lg:btn-ghost lg:text-xl">Cade Lueker</a>
+                </div>
+                <div class="lg:hidden navbar-end">
+                    <a class="lm-2 btn btn-ghost text-xl">Cade Lueker</a>
+                </div>
+                <div class="navbar-end hidden lg:ml-auto lg:flex">
+                    <ul class="menu menu-horizontal px-1">
+                        <li><a @click="scrollToComponent('about')">About</a></li>
+                        <li><a @click="scrollToComponent('projects')">Projects</a></li>
+                        <li><a @click="scrollToComponent('resume')">Resume</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="drawer-side z-10">
+            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label> 
+            <ul class="menu p-4 flex min-h-full bg-base-200">
+                <a @click="scrollToComponent('about')" class="btn"><li>About</li></a>
+                <a @click="scrollToComponent('projects')" class="btn"><li>Projects</li></a>
+                <a @click="scrollToComponent('resume')" class="btn"><li>Resume</li></a>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <style scoped>
