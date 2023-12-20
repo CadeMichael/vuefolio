6e866415e8af0014645841e75d0a728fbf49ce86
Cade Lueker <cade@debPad.Home>
2023-12-19 18:26:27 -0700

    adding to the /About/ Section

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
 <template>
 	<div class="container lg:m-4 mx-auto w-full">
 		<p class="text-xl pl-2 mb-6 lg:mx-0 mx-4">Hi! My name is Cade Lueker. I'm a software engineer from NYC living in Denver. My passions are Linux, Python, Rust, Vue & JS, and Jiu Jitsu. I spend my free time studying programming languages and verification, doing little toy projects, and teaching Braziliain Jiu Jitsu.</p>
-		<ol class="ml-4 relative border-s border-gray-200 dark:border-gray-700">                  
+		<ol class="ml-4 relative border-s border-gray-200 dark:border-gray-700">
 			<li class="mb-4 ms-4">
 				<div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
 				<time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">August 2022</time>
@@ -13,7 +13,9 @@
 						<h3 class="text-lg font-semibold text-info">Software Engineer at Publicis Sapient</h3>
 					</div>
 					<div class="collapse-content">
-						<p class="text-base font-normal text-gray-500 dark:text-gray-400">After Graduating I moved to Denver, CO and began working fulltime as a software engineer. Working mainly with JavaScript, Apex, and Salesforce.</p>
+                                            <p class="text-base font-normal text-gray-500 dark:text-gray-400">- After Graduating I moved to Denver, CO and began working fulltime as a software engineer. Working mainly with JavaScript, Apex, and Salesforce.</p>
+                                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">- I obtained the <b>Salesforce JavaScript Developer Certification</b>, through self study and experience working with both vanilla JS and the Salesforce Lightning web framework to build out front ends within the salesforce ecosystem. For several projects I was tasked with writing Apex code (a wrapper around Java) to make and process API endpoint calls which were then relayed to a front end.</p>
+                                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">- I was also responsible for writing tests, both unit tests and test scripts. This built upon my experience from being a test framework engineer at Bluescape and aided in my ability to write clean, maintainable code</p>
 					</div>
 				</div>
 			</li>
@@ -25,7 +27,18 @@
 						<h3 class="text-lg font-semibold text-info">BA in Computer Science</h3>
 					</div>
 					<div class="collapse-content">
-						<p class="text-base font-normal text-gray-500 dark:text-gray-400">I graduated from Case Western Reserve University, majoring in computer science with a 3.81 GPA</p>
+                                            <p class="text-base font-normal text-gray-500 dark:text-gray-400">I graduated from Case Western Reserve University, majoring in computer science with a <b>3.81 GPA</b>. I studdied abroad at Yonsei University in Seoul, Korea my junior year. I was able to continue my CS coursework abroad, making sure that I was able to graduate on time. At Case Western I worked as a TA the first semester of my senior year for the course "Introduction to Object Oriented Programming". I wrestled on the DIII varsity team for a year and a half before the Covid pandemic and when we returned to normal courses I started the Case Grappling club.</p>
+                                            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Relevant Coursework</p>
+                                            <ul class="text-base font-normal text-gray-500 dark:text-gray-400">
+                                                <li>- Data Scructures</li>
+                                                <li>- Algorithms</li>
+                                                <li>- Data Science</li>
+                                                <li>- Databases</li>
+                                                <li>- Programming Languages</li>
+                                                <li>- Discrete Mathematics</li>
+                                                <li>- Linear Algebra</li>
+                                                <li>- Web Development</li>
+                                            </ul>
 					</div>
 				</div>
 			</li>
@@ -37,7 +50,9 @@
 						<h3 class="text-lg font-semibold text-info">Bluescape Test Engineer Intern</h3>
 					</div>
 					<div class="collapse-content">
-						<p class="text-base font-normal text-gray-500 dark:text-gray-400">During my senior year of undergrad I began a fulltime internship with Bluescape, working on their testing infrastructure. Writing and monitoring Javascript tests for their front end products.</p>
+						<p class="text-base font-normal text-gray-500 dark:text-gray-400">- During my senior year of undergrad I began a fulltime internship with Bluescape, working on their testing infrastructure. Writing and monitoring Javascript tests for their front end products.</p>
+                                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">- This work included working with the Javascript test frameworks <b>Jest & Puppeteer</b> in addition to Jira and GitHub deployments</p>
+                                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">- I monitored Jira for issues with the written tests and or bugs in the front end. If a test was failing I had to confirm that the front end was indeed faulty or that the test needed to be updated. If a test needed to be written or updated I documented my code and ran test deployments that when passed would get deployed into production</p>
 					</div>
 				</div>
 			</li>
@@ -49,7 +64,8 @@
 						<h3 class="text-lg font-semibold text-info">Flutter Developer Intern</h3>
 					</div>
 					<div class="collapse-content">
-						<p class="text-base font-normal text-gray-500 dark:text-gray-400">While studying abroad I worked remotely for GXC, LLC. Spending 5 months developing their mobile interface with Flutter and Dart.</p>
+						<p class="text-base font-normal text-gray-500 dark:text-gray-400">- While studying abroad I worked remotely for GXC, LLC. Spending 5 months developing their mobile interface with Flutter and Dart.</p>
+						<p class="text-base font-normal text-gray-500 dark:text-gray-400">- My main tasks were using the flutter http api to call the backend for content and dynamically render it on the flutter front end. Additionally I was tasked with researching ways to integrate speech to text into the application</p>
 					</div>
 				</div>
 			</li>
@@ -61,7 +77,8 @@
 						<h3 class="text-lg font-semibold text-info">Computer Science as a Career</h3>
 					</div>
 					<div class="collapse-content">
-						<p class="text-base font-normal text-gray-500 dark:text-gray-400">After working as an undergraduate research assistant and helping researchers analyze experimental data, I decided to switch my career aspiration to CS. I built a PC, started using Linux, and switched all coursework to CS.</p>
+						<p class="text-base font-normal text-gray-500 dark:text-gray-400">- After working as an undergraduate research assistant and helping researchers analyze experimental data, I decided to switch my career aspiration to CS. I built a PC, started using Linux, and switched all coursework to CS.</p>
+                                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">- Through my CS journey I fell in love with Open Source and started using Vim -> Emacs -> Neovim. I make youtube content on programming and the editors that I am using. I have made several Neovim plugins using Lua for personal use, and one for the Zig programming language that has been starred around 19 times on GitHub (so I hope some people are using it aswell!). Computer Science has become a huge part of my life and my goal is to keep learning and programming daily</p>
 					</div>
 				</div>
 			</li>
