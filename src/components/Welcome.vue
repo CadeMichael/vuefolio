9aa6500540e25f7e353dceb617a27a4aa87cb00a
Cade Michael <cade.lueker@gmail.com>
2024-01-02 18:14:30 -0700

    shadow

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,7 +2,7 @@
     <div class="h-screen hero rounded-xl flex flex-col justify-center items-center">
         <div class="hero-content mx-auto grid lg:grid-cols-2 lg:-mt-10 md:grid-cols-1 gap-4">
             <div class="avatar">
-                <div class="w-3/4 rounded-xl mr-auto ml-auto">
+                <div class="w-3/4 rounded-xl mr-auto ml-auto shadow-2xl">
                     <img src="https://image-aws-us-west-2.vsco.co/af4352/91324749/5dd05f650110271b358d3cee/vsco5dd05f66e9144.jpg" />
                 </div>
             </div>
